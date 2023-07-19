4b2b8defd50e1743e3e7ebf665fefdc0b0bc9ef8
andy <56748572+hzq-up@users.noreply.github.com>
2023-07-19 18:20:13 +0800

    update: 增加时间线页面

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
+import styles from './app.module.scss'
 
 function App() {
 
   return (
-    <>
+    <div className={styles.container}>
+      <h2>时间线</h2>
+      <p className={styles.desc}>这是一个时间线组件的展示!</p>
+      <div className={styles.timeLineBox}>
 
-    </>
+      </div>
+    </div>
   )
 }
 
---
+++ src/components/TimeLine/index.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+
+function TimeLine() {
+
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default TimeLine
