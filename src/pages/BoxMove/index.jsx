41455da92e6cd8f9049360e10ee5deaf0f97523b
andy <56748572+hzq-up@users.noreply.github.com>
2023-07-27 19:38:54 +0800

    feat: 新增四脚线框跟随鼠标移动功能

--- src/pages/BoxMove/index.jsx
+++ src/pages/BoxMove/index.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useRef } from 'react'
 import styles from './index.module.scss'
-import boxImg from '@/assets/images/box.jpg'
 function BoxMove() {
-
+  useEffect(() => {
+    const imgContainer = document.querySelector(`.${styles.gridContainer}`)
+    const imgs = document.querySelectorAll(`.${styles.boxImg} img`)
+    const box = document.querySelector(`.${styles.box}`)
+    for (const img of imgs) {
+      img.onmouseenter = (e) => {
+        const imgWidth = e.target.offsetWidth;
+        const x = e.target.offsetLeft;
+        const y = e.target.offsetTop;
+        console.log(x, y)
+        console.log(y - styles.gap.replace('px') + 'px')
+        box.style.setProperty('top', y - styles.gap.replace('px', '') + 'px')
+        box.style.setProperty('left', x - styles.gap.replace('px', '') + 'px')
+        // const lineLength = 
+      }
+    }
+    // .onmouseenter = () => {
+    //   console.log('aa')
+    // }
+  }, [])
 
   return (
     <div className={styles.container}>
-      <div className={styles.boxImg}>
-        <img src={boxImg}></img>
+      <div className={styles.gridContainer}>
+        {
+          Array.from(new Array(9)).map((item, i) => {
+            return <div key={i} className={styles.boxImg}>
+              <img src={new URL(`@/assets/images/a1.jpg`, import.meta.url).href}></img>
+            </div>
+          })
+        }
         <div className={styles.box}>
 
         </div>
