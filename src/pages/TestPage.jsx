e77855c691aca97b6c49bee40f7dbc2c5966289c
andy <56748572+hzq-up@users.noreply.github.com>
2023-07-28 18:03:04 +0800

    测试

--- src/pages/TestPage.jsx
+++ src/pages/TestPage.jsx
@@ -1,9 +1,26 @@
 export default function Index() {
 
 
+  let lastExecutionTime = Date.now();
+  const interval = 1000; // 定时器的执行频率为1秒
+  
+  const doSomething = () => {
+    // 执行你的定时任务
+    console.log('执行定时任务');
+  
+    // 更新上次执行时间
+    lastExecutionTime = Date.now();
+  
+    // 使用 requestAnimationFrame 来触发下一次定时任务
+    requestAnimationFrame(doSomething);
+  };
+  
+  // 开始执行定时任务
+  // requestAnimationFrame(doSomething);
+
   return (
     <div >
-      测试页面
+      测试切换tab页面任然执行定时任务
     </div>
   )
 }
